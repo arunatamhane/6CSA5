36f7004aa8c9e3a34f6e97f609a7d76a76bbfb3e
ADMIN <ADMIN@DESKTOP-M8PQBEB>
2023-06-08 13:13:07 +0530

    practice

---
+++ 6CSA5/src/Assignment/Example.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Example {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://fb.com");
+//		driver.quit();
+	}
+
+}
